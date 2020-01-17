5daef7d48382d5c2d162d494ade4b48f3ef5e35b
cardioid1996 <892144672@qq.com>
2020-01-17 19:45:41 +0800

    封装了CookieUtil，修改了用户登录功能

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -5,6 +5,7 @@ import com.mmall.common.RedisPool;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPoolUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.mmall.pojo.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
@@ -30,11 +34,14 @@ public class UserController {
 
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody // Jackson直接将返回值转换为json格式
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         // service --> dao
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
             //session.setAttribute(Const.CURRENT_USER, response.getData());
+            CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+            CookieUtil.readLoginToken(httpServletRequest);
+            CookieUtil.delLoginToken(httpServletRequest, httpServletResponse);
             RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()), Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
         return response;
--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -1,4 +1,55 @@
 package com.mmall.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Slf4j
 public class CookieUtil {
+    private final static String COOKIE_DOMAIN = ".happymmall.com";
+    private final static String COOKIE_NAME = "mmall_login_token";
+
+    public static String readLoginToken(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null){
+            for (Cookie cookie : cookies){
+                log.info("read cookiename:{}, cookievalue:{}", cookie.getName(), cookie.getValue());
+                if (StringUtils.equals(cookie.getName(), COOKIE_NAME)){
+                    log.info("return cookiename: {}, cookieValue:{}",cookie.getName(), cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void writeLoginToken(HttpServletResponse response, String token){
+        Cookie cookie = new Cookie(COOKIE_NAME, token);
+        cookie.setDomain(COOKIE_DOMAIN);
+        cookie.setPath("/");
+        // 如果maxAge不设置，cookie就不会写入硬盘，而是写在内存，只在当前界面有效
+        cookie.setMaxAge(60 * 60 * 24 * 365);  // 设为一年，-1代表永久
+        log.info("write cookiename: {}, cookieValue:{}", cookie.getName(), cookie.getValue());
+        response.addCookie(cookie);
+    }
+
+    public static void delLoginToken(HttpServletRequest request, HttpServletResponse response){
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null){
+            for (Cookie cookie : cookies){
+                if (StringUtils.equals(cookie.getName(), COOKIE_NAME)){
+                    cookie.setDomain(COOKIE_DOMAIN);
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);  // maxAge设为0表示删除该cookie
+                    response.addCookie(cookie);
+                    return;
+                }
+            }
+        }
+    }
+
 }
